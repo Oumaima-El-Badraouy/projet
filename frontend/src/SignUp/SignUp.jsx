75a8e9d2abbd11198acbff397af0a4413f13592b
Abdelhadi128 <151454902+Abdelhadi128@users.noreply.github.com>
2025-03-10 22:25:53 +0000

    create sign up frontend

---
+++ frontend/src/SignUp/SignUp.jsx
@@ -0,0 +1,116 @@
+import React, { useState } from 'react'
+
+export default function SingUp () {
+    
+    // state containe the inputs values
+    const [userData, setUserDta] = useState({
+        schoolName:'',
+        email:'',
+        password:'',
+        phonNumber:'',
+    })
+
+    // state containe the value of comformation password
+    const [validPassword, setValidPassword] = useState('')
+
+    // state containe the errors of validation inputs
+    const [errors, setErrors] = useState({
+        schoolName:'',
+        email:'',
+        password:'',
+        phonNumber:'',
+    })
+
+    // regex of email
+    const validateEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+    // regex of phone number
+    const validPhoneNumner = /[0-9]{10}/
+
+    //  check the values of the inputs
+    const handleData = (e)=>{
+        e.preventDefault()
+        let validForm = true
+        if(userData.schoolName.trim() === ''){
+            setErrors((prevState)=>({...prevState, schoolName:'Le nom de l\'école oubligatoire'}))
+        }else{
+            setErrors((prevState)=>({...prevState, schoolName:''}))
+        }
+        if(userData.email.trim() === ''){
+            setErrors((prevState)=>({...prevState, email:'L\'émail oubligatoire'}))
+        }else if(!userData.email.match(validateEmail)){
+            setErrors((prevState)=>({...prevState, email:"L'email n'est pas valide"}))
+        }else{
+            setErrors((prevState)=>({...prevState, email:''}))
+        }
+        if(userData.password.trim() === ''){
+            setErrors((prevState)=>({...prevState, password:'Mot de passe oubligatoire'}))
+        }else if(userData.password.trim().length < 8){
+            setErrors((prevState)=>({...prevState, password:'Le mot de passe doit contenir au moins 8 caractères'}))
+        }else{
+            setErrors((prevState)=>({...prevState, password:''}))
+        }
+        if(userData.phonNumber.trim() === ''){
+            setErrors((prevState)=>({...prevState, phonNumber:'Numéro de téléphone oubligatoire'}))
+        }else if(!userData.phonNumber.trim().match(validPhoneNumner)){
+            setErrors((prevState)=>({...prevState, phonNumber:'Numéro de téléphone n\'est pas valide'}))
+        }else{
+            setErrors((prevState)=>({...prevState, phonNumber:''}))
+        }
+    }
+
+    return (
+        <div>
+            <div className='h-screen lg:flex md:block sm:block items-center justify-around'>
+                <div className='lg:w-1/2 xl:w-1/2 2xl:w-1/2 md:w-full sm:w-full text-center h-1/2 grid place-items-center'>
+                    <div>
+                    <h1 className='mb-4 text-4xl text-blue-700 font-extrabold leading-none tracking-tight md:text-5xl lg:text-5xl dark:text-white'>
+                            DALLEEL
+                        </h1>
+                        <p className='lg:text-3xl text-2xl font-bold'>
+                            Ensemble vers un meilleur avenir !
+                        </p>
+                    </div>
+                </div>
+                <div className='lg:w-1/2 xl:w-1/2 2xl:w-1/2 w-full pb-4'>
+                    <form className="w-11/12 lg:w-9/12 mx-auto" onSubmit={handleData}> 
+                        <div>
+                            <h1 className='mb-6 text-xl font-bold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl dark:text-white'> Créer un compt</h1>
+                        </div>
+                        <div className="relative z-0 w-full mb-5 group">
+                            <input type="text" onChange={(e)=>{setUserDta((prevState)=>({...prevState, email:e.target.value}))}} name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "  />
+                            <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
+                            <p className='text-red-600'>{errors.email}</p>
+                        </div>
+                        <div className="relative z-0 w-full mb-5 group">
+                            <input type="password" onChange={(e)=>{setUserDta((prevState)=>({...prevState, password:e.target.value}))}} name="floating_password" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "  />
+                            <label htmlFor="floating_password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Mot de passe</label>
+                            <p className='text-red-600'>{errors.password}</p>
+                        </div>
+                        <div className="relative z-0 w-full mb-5 group">
+                            <input type="password" onChange={(e)=>{setValidPassword(e.target.value)}} name="repeat_password" id="floating_repeat_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "  />
+                            <label htmlFor="floating_repeat_password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirmé mot de passe</label>
+                            <p className='text-red-600'>{userData.password !== validPassword && 'Les mots de passe ne correspondent pas.'}</p>
+                        </div>
+                        <div className="grid md:grid-cols-2 md:gap-6">
+                            <div className="relative z-0 w-full mb-5 group">
+                                <input type="text" onChange={(e)=>{setUserDta((prevState)=>({...prevState, schoolName:e.target.value}))}} name="floating_first_name" id="floating_first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "  />
+                                <label htmlFor="floating_first_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Nom d'école</label>
+                                <p className='text-red-600'>{errors.schoolName}</p>
+                            </div>
+                            <div className="relative z-0 w-full mb-5 group">
+                                <input type="tel" onChange={(e)=>{setUserDta((prevState)=>({...prevState, phonNumber:e.target.value}))}} name="floating_phone" id="floating_phone" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "  />
+                                <label htmlFor="floating_phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Numéro téléphone (+212 00-0000-000)</label>
+                                <p className='text-red-600'>{errors.phonNumber}</p>
+                            </div>
+                        </div>
+                        <p className='mb-3'>
+                            Déjà inscrit ? <span className='text-blue-600'>Se connecter</span>
+                        </p>
+                        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+<<<<<<< HEAD
 import Login from './Components/Login.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
   
     <App/>
+=======
+import First from './first.jsx'
+import Home from './Home/home.jsx'
+import SingUp from './SignUp/SignUp.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <Home/> */}
+    <SingUp/>
+>>>>>>> 02e0847 (frontend sign Up)
   </StrictMode>,
 )
